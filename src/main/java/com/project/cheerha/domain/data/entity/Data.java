6b3bbc7f9489ecabc991f98b921aa1eb00e1720b
“llRosell” <llovein0920@gmail.com>
2025-02-11 22:06:16 +0900

    ✨ feat : data entity에 position, count추가 및 키워드를 리스트로 반환하는 getRequiredSkills 메서드와 채용기간 반환하는 getHiringPeriod메서드 생성

--- src/main/java/com/project/cheerha/domain/data/entity/Data.java
+++ src/main/java/com/project/cheerha/domain/data/entity/Data.java
@@ -1,18 +1,21 @@
 package com.project.cheerha.domain.data.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.project.cheerha.domain.keyword.entity.DataKeyword;
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "data")
 public class Data {
 
@@ -48,4 +51,49 @@ public class Data {
 
     // todo tinyint일 때 뭘 써야 좋을지 추후 수정 예정
     private int career;
+
+    @Column(length = 255, nullable = false)
+    private String position;  // 포지션 (직무명 예시: SW개발)
+
+    private int count;
+
+    // Data와 Keyword를 연결하는 DataKeyword 중간 테이블을 사용하여 자격 요건 관리
+    @OneToMany(mappedBy = "data", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<DataKeyword> dataKeywords = new HashSet<>();  // 자격 요건(키워드 리스트)
+
+    public Data(String title, String company, String location, Integer salary, String jobType, String url,
+                LocalDate hiringStartPeriod, LocalDate hiringEndPeriod, String education, Integer career,
+                String position, Set<DataKeyword> dataKeywords) {
+        this.title = title;
+        this.company = company;
+        this.location = location;
+        this.salary = salary;
+        this.jobType = jobType;
+        this.url = url;
+        this.hiringStartPeriod = hiringStartPeriod;
+        this.hiringEndPeriod = hiringEndPeriod;
+        this.education = education;
+        this.career = career;
+        this.position = position;
+        this.dataKeywords = dataKeywords;
+    }
+
+    // 자격 요건 키워드 리스트 반환
+    public Set<String> getRequiredSkills() {
+        Set<String> skills = new HashSet<>();
+        for (DataKeyword dataKeyword : dataKeywords) {
+            skills.add(dataKeyword.getKeyword().getName());
+        }
+        return skills;
+    }
+
+    // 채용 기간을 문자열로 반환하는 메서드
+    public String getHiringPeriod() {
+        if (hiringStartPeriod != null && hiringEndPeriod != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            return hiringStartPeriod.format(formatter) + " ~ " + hiringEndPeriod.format(formatter);
+        } else {
+            return "채용 기간 정보 없음";
+        }
+    }
 }
