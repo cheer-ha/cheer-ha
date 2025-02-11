8a797396a4082d2ad6a7a82b5adbb09c398c1ebe
joyangjin <wndid2008@gmail.com>
2025-02-11 20:46:35 +0900

    :sparkles:add redirect

--- src/main/java/com/project/cheerha/domain/data/controller/DataController.java
+++ src/main/java/com/project/cheerha/domain/data/controller/DataController.java
@@ -1,12 +1,25 @@
 package com.project.cheerha.domain.data.controller;
 
+import com.project.cheerha.domain.data.entity.Data;
+import com.project.cheerha.domain.data.repository.DataRepository;
+import com.project.cheerha.domain.data.service.DataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
-@RequestMapping
+@RequestMapping("/data")
 @RestController
 @RequiredArgsConstructor
 public class DataController {
 
+    private final DataService dataService;
+
+    @GetMapping("/{id}")
+    public RedirectView redirectView (@PathVariable Long id) {
+        String url = dataService.urlFindById(id);
+        return new RedirectView(url);
+    }
 }
--- src/main/java/com/project/cheerha/domain/data/service/DataService.java
+++ src/main/java/com/project/cheerha/domain/data/service/DataService.java
@@ -1,5 +1,11 @@
 package com.project.cheerha.domain.data.service;
 
+import com.project.cheerha.common.exception.BaseException;
+import com.project.cheerha.common.exception.ErrorCode;
+import com.project.cheerha.domain.data.entity.Data;
+import com.project.cheerha.domain.data.repository.DataRepository;
+import java.util.Base64;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,4 +13,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DataService {
 
+    private final DataRepository dataRepository;
+
+    public String urlFindById(Long id) {
+        Data data = dataRepository.findById(id).orElseThrow();
+        String url = data.getUrl();
+
+        if (!url.startsWith("http")) {
+            url = "https://" + url;
+        }
+
+        System.out.println("🔍 최종 리다이렉트 URL: " + url);
+        return url;
+    }
 }
