0cb76c53bd71ccb192bb4d0361d56a22834d3fd8
roqkfchqh <plmokn48@daum.net>
2025-02-11 16:38:25 +0900

    ✨ feat: GET /user 구현

--- src/main/java/com/project/cheerha/domain/auth/dto/response/CreateLoginResponseDto.java
+++ src/main/java/com/project/cheerha/domain/auth/dto/response/CreateLoginResponseDto.java
@@ -4,7 +4,6 @@ public record CreateLoginResponseDto(
     String message,
     String token
 ) {
-
     public static CreateLoginResponseDto of(String token) {
         return new CreateLoginResponseDto("로그인 성공", token);
     }
