15dd665722780d6f9e67fd95ff6a3623c70323ef
roqkfchqh <plmokn48@daum.net>
2025-02-11 17:15:52 +0900

    🐛 fix: configuration 속성 중복 오류 수정

--- src/main/java/com/project/cheerha/common/properties/JwtSecurityProperties.java
+++ src/main/java/com/project/cheerha/common/properties/JwtSecurityProperties.java
@@ -3,11 +3,9 @@ package com.project.cheerha.common.properties;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter //setter 없으면 설정 안 돌아감
-@Configuration
 @ConfigurationProperties(prefix = "jwt")
 public class JwtSecurityProperties {
 
