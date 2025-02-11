517d4427f397779b1552e3c65cc1981a9bc7483a
“llRosell” <llovein0920@gmail.com>
2025-02-11 22:09:31 +0900

    ✨ feat	: ReadBookmarkResponseDto 추가

---
+++ src/main/java/com/project/cheerha/domain/bookmark/dto/ReadBookmarkResponseDto.java
@@ -0,0 +1,23 @@
+package com.project.cheerha.domain.bookmark.dto;
+
+import com.project.cheerha.domain.bookmark.entity.Bookmark;
+import com.project.cheerha.domain.data.entity.Data;
+
+import java.util.Set;
+
+public record ReadBookmarkResponseDto(
+        String company,       // 회사명
+        String hiringPeriod,  // 채용 기간
+        String position,      // 포지션 (직무명)
+        Set<String> requiredSkills // 자격 요건 (기술 키워드 리스트)
+) {
+    public static ReadBookmarkResponseDto fromEntity(Bookmark bookmark) {
+        Data data = bookmark.getData();
+        return new ReadBookmarkResponseDto(
+                data.getCompany(),           // 회사명
+                data.getHiringPeriod(),      // 채용 기간
+                data.getPosition(),          // 포지션 (직무명)
+                data.getRequiredSkills()     // 자격 요건 (기술 키워드 리스트)
+        );
+    }
+}
