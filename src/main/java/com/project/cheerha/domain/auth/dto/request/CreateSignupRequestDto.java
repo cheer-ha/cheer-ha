87e9d803e09ce20dfdcbf59eb13b6980095cf9e1
roqkfchqh <plmokn48@daum.net>
2025-02-11 15:10:15 +0900

    🐛 fix: int에 notblank -> notnull로 수정

--- src/main/java/com/project/cheerha/domain/auth/dto/request/CreateSignupRequestDto.java
+++ src/main/java/com/project/cheerha/domain/auth/dto/request/CreateSignupRequestDto.java
@@ -2,13 +2,14 @@ package com.project.cheerha.domain.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateSignupRequestDto(
     @Email String email,
     @NotBlank String password,
     @NotBlank String name,
-    @NotBlank int age,
-    @NotBlank int career
+    @NotNull int age,
+    @NotNull int career
 ) {
 
 }
