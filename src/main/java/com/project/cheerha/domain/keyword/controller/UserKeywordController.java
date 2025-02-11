accf9dace84b8bed3cf2cfee043d5732271926f9
backswan0 <rnrwprh3434@gmail.com>
2025-02-11 16:02:29 +0900

    :sparkles: feat: 사용자 채용 키워드 알림 등록 기능 구현
    - [필요] dev git pull 이후 사용자 정보 가져오는 로직 수정 및 테스트 예정

--- src/main/java/com/project/cheerha/domain/keyword/controller/UserKeywordController.java
+++ src/main/java/com/project/cheerha/domain/keyword/controller/UserKeywordController.java
@@ -1,6 +1,15 @@
 package com.project.cheerha.domain.keyword.controller;
 
+import com.project.cheerha.domain.keyword.dto.request.CreateUserKeywordRequestDto;
+import com.project.cheerha.domain.keyword.dto.response.CreateUserKeywordResponseDto;
+import com.project.cheerha.domain.keyword.service.UserKeywordService;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserKeywordController {
 
+    private final UserKeywordService userKeywordService;
+
+
+    @PostMapping("/user/keywords")
+    public ResponseEntity<List<CreateUserKeywordResponseDto>> registerUserKeywordList(
+        @RequestBody List<CreateUserKeywordRequestDto> requestDtoList
+    ) {
+        List<CreateUserKeywordResponseDto> responseDtoList = new ArrayList<>();
+
+        responseDtoList = userKeywordService.createUserKeyword(requestDtoList);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDtoList);
+    }
 }
--- src/main/java/com/project/cheerha/domain/keyword/service/UserKeywordService.java
+++ src/main/java/com/project/cheerha/domain/keyword/service/UserKeywordService.java
@@ -1,10 +1,52 @@
 package com.project.cheerha.domain.keyword.service;
 
+import com.project.cheerha.domain.keyword.dto.request.CreateUserKeywordRequestDto;
+import com.project.cheerha.domain.keyword.dto.response.CreateUserKeywordResponseDto;
+import com.project.cheerha.domain.keyword.entity.Keyword;
+import com.project.cheerha.domain.keyword.entity.UserKeyword;
+import com.project.cheerha.domain.keyword.repository.KeywordRepository;
+import com.project.cheerha.domain.keyword.repository.UserKeywordRepository;
+import com.project.cheerha.domain.user.entity.User;
+import com.project.cheerha.domain.user.repository.UserRepository;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserKeywordService {
 
+    private final KeywordRepository keywordRepository;
+    private final UserKeywordRepository userKeywordRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public List<CreateUserKeywordResponseDto> createUserKeyword(
+        List<CreateUserKeywordRequestDto> requestDtoList
+    ) {
+       // todo dev git pull한 다음 수정 예정
+        long userId = 1L;
+
+        User foundUser = userRepository.findById(userId).orElseThrow();
+
+        List<CreateUserKeywordResponseDto> responseDtoList = new ArrayList<>();
+
+        responseDtoList = requestDtoList.stream()
+            .map(dto ->
+                {
+                    Keyword foundKeyword = keywordRepository.findById(dto.keywordId()).orElseThrow();
+
+                    UserKeyword userKeyword = UserKeyword.of(foundUser, foundKeyword);
+
+                    userKeywordRepository.save(userKeyword);
+
+                    return CreateUserKeywordResponseDto.of(userKeyword);
+
+                }
+            ).toList();
+
+        return responseDtoList;
+    }
 }
