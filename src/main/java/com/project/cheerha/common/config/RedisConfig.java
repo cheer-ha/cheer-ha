d4ad75e062b94b34b80411c427549770bb1ec8c9
roqkfchqh <plmokn48@daum.net>
2025-02-12 01:25:16 +0900

    🎉 add: redisConfig, redis 설정 추가 lettuce기반 connection 사용

---
+++ src/main/java/com/project/cheerha/common/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.project.cheerha.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("localhost", 6379);
+    }
+
+    @Bean
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        return new StringRedisTemplate(redisConnectionFactory);
+    }
+}
