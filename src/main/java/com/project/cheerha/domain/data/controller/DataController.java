4fae7a91ccb11127199df5cdbc238b2fdf69df3f
tmdcksdl <tmdcksdl99@naver.com>
2025-02-12 22:42:26 +0900

    ♻️ refactor: 파라미터 및 페이징 방법 변경
    - @ModelAttribute 사용
    - 페이징 검증 추가

--- src/main/java/com/project/cheerha/domain/data/controller/DataController.java
+++ src/main/java/com/project/cheerha/domain/data/controller/DataController.java
@@ -1,19 +1,17 @@
 package com.project.cheerha.domain.data.controller;
 
+import com.project.cheerha.common.exception.CustomException;
+import com.project.cheerha.common.exception.ErrorCode;
+import com.project.cheerha.domain.data.dto.request.ReadDataRequestDto;
 import com.project.cheerha.domain.data.dto.response.ReadDataResponseDto;
 import com.project.cheerha.domain.data.service.DataService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/search")
 @RestController
@@ -24,25 +22,31 @@ public class DataController {
 
     @GetMapping
     public ResponseEntity<Page<ReadDataResponseDto>> readData(
-        @RequestParam(required = false) String education,
-        @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime hiringStartPeriod,
-        @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime hiringEndPeriod,
-        @RequestParam(required = false) String location,
-        @RequestParam(required = false) Integer career,
-        @RequestParam(required = false) String jobType,
-        @RequestParam(required = false) String requiredSkill,
-        Pageable pageable
+            @ModelAttribute ReadDataRequestDto requestDto,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size
     ) {
+        Pageable pageable = validatePageSize(page, size);
+
         Page<ReadDataResponseDto> dtoPage = dataService.readData(
-            education,
-            hiringStartPeriod,
-            hiringEndPeriod,
-            location,
-            career,
-            jobType,
-            requiredSkill,
-            pageable);
+            requestDto.getEducation(),
+            requestDto.getHiringStartPeriod(),
+            requestDto.getHiringEndPeriod(),
+            requestDto.getLocation(),
+            requestDto.getCareer(),
+            requestDto.getJobType(),
+            requestDto.getRequiredSkill(),
+            pageable
+            );
 
         return ResponseEntity.status(HttpStatus.OK).body(dtoPage);
+    }
+
+    private Pageable validatePageSize(int page, int size) {
+        if (page < 1 || size < 1) {
+            throw new CustomException(ErrorCode.PAGING_ERROR);
+        }
+
+        return PageRequest.of(page - 1, size);
     }
 }
