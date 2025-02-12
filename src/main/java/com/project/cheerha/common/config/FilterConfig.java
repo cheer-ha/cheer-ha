d6ac42294970a76d43c7561f71ca9f767d304c45
lcy <plmokn48@daum.net>
2025-02-12 11:54:45 +0900

    🐛 fix: jwtUtil에서 생성자 변경으로 인한 오류 수정

--- src/main/java/com/project/cheerha/common/config/FilterConfig.java
+++ src/main/java/com/project/cheerha/common/config/FilterConfig.java
@@ -1,5 +1,6 @@
 package com.project.cheerha.common.config;
 
+import com.project.cheerha.common.properties.JwtSecurityProperties;
 import com.project.cheerha.common.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
@@ -11,11 +12,12 @@ import org.springframework.context.annotation.Configuration;
 public class FilterConfig {
 
     private final JwtUtil jwtUtil;
+    private final JwtSecurityProperties jwtSecurityProperties;
 
     @Bean
     public FilterRegistrationBean<JwtFilter> jwtFilter() {
         FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new JwtFilter(jwtUtil));
+        registrationBean.setFilter(new JwtFilter(jwtSecurityProperties, jwtUtil));
         registrationBean.addUrlPatterns("/*");
 
         return registrationBean;
