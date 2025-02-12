8af115322105a180bcb96c4608ad48a8ffb6e284
“llRosell” <llovein0920@gmail.com>
2025-02-12 19:35:14 +0900

    ♻️ refactor : dataId에 @NotNull 추가, toEntity 메서드는 Entity 클래스로 이동

--- src/main/java/com/project/cheerha/domain/bookmark/dto/CreateBookmarkRequestDto.java
+++ src/main/java/com/project/cheerha/domain/bookmark/dto/CreateBookmarkRequestDto.java
@@ -1,14 +1,10 @@
 package com.project.cheerha.domain.bookmark.dto;
 
-import com.project.cheerha.domain.bookmark.entity.Bookmark;
-import com.project.cheerha.domain.data.entity.Data;
-import com.project.cheerha.domain.user.entity.User;
+import jakarta.validation.constraints.NotNull;
 
 public record CreateBookmarkRequestDto(
+        @NotNull
         Long dataId
 ) {
-    // userId로 User를 찾아서 Bookmark 엔티티 생성
-    public Bookmark toEntity(User user, Data data) {
-        return new Bookmark(user, data);  // user와 data를 이용해 Bookmark 엔티티 생성
-    }
+
 }
