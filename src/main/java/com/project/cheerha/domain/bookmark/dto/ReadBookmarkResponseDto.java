0b723f1a8f4f1065a3db02729c5541adcd6ee7aa
“llRosell” <llovein0920@gmail.com>
2025-02-12 19:35:44 +0900

    ♻️ refactor : set -> List로 변경

--- src/main/java/com/project/cheerha/domain/bookmark/dto/ReadBookmarkResponseDto.java
+++ src/main/java/com/project/cheerha/domain/bookmark/dto/ReadBookmarkResponseDto.java
@@ -3,21 +3,22 @@ package com.project.cheerha.domain.bookmark.dto;
 import com.project.cheerha.domain.bookmark.entity.Bookmark;
 import com.project.cheerha.domain.data.entity.Data;
 
+import java.util.List;
 import java.util.Set;
 
 public record ReadBookmarkResponseDto(
         String company,       // 회사명
         String hiringPeriod,  // 채용 기간
         String position,      // 포지션 (직무명)
-        Set<String> requiredSkills // 자격 요건 (기술 키워드 리스트)
+        List<String> requiredSkillList // 자격 요건 (기술 키워드 리스트)
 ) {
-    public static ReadBookmarkResponseDto fromEntity(Bookmark bookmark) {
+    public static ReadBookmarkResponseDto toDto(Bookmark bookmark) {
         Data data = bookmark.getData();
         return new ReadBookmarkResponseDto(
                 data.getCompany(),           // 회사명
                 data.getHiringPeriod(),      // 채용 기간
                 data.getPosition(),          // 포지션 (직무명)
-                data.getRequiredSkills()     // 자격 요건 (기술 키워드 리스트)
+                data.getRequiredSkillList()     // 자격 요건 (기술 키워드 리스트)
         );
     }
 }
