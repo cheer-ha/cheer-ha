0cd2d3396c760c211d8a3512e579c71d9d82f7d6
tmdcksdl <tmdcksdl99@naver.com>
2025-02-12 14:40:52 +0900

    ✨ feat: 데이터 조회 기능 구현

--- src/main/java/com/project/cheerha/domain/data/controller/DataController.java
+++ src/main/java/com/project/cheerha/domain/data/controller/DataController.java
@@ -1,12 +1,49 @@
 package com.project.cheerha.domain.data.controller;
 
+import com.project.cheerha.domain.data.dto.response.ReadDataResponseDto;
+import com.project.cheerha.domain.data.service.DataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+import java.time.LocalDate;
+
+@RequestMapping("/search")
 @RestController
 @RequiredArgsConstructor
 public class DataController {
 
+    private final DataService dataService;
+
+    @GetMapping
+    public ResponseEntity<Page<ReadDataResponseDto>> readData(
+        @RequestParam(required = false) String education,
+        @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate hiringStartPeriod,
+        @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate hiringEndPeriod,
+        @RequestParam(required = false) String location,
+        // TODO : int 타입을 Integer 타입으로 변경해서 null 값을 처리할 수 있도록 하면 어떨까요?
+        @RequestParam(required = false) int career,
+        @RequestParam(required = false) String jobType,
+        @RequestParam(required = false) String requiredSkill,
+        Pageable pageable
+    ) {
+        Page<ReadDataResponseDto> dtoPage = dataService.readData(
+            education,
+            hiringStartPeriod,
+            hiringEndPeriod,
+            location,
+            career,
+            jobType,
+            requiredSkill,
+            pageable);
+
+        return ResponseEntity.status(HttpStatus.OK).body(dtoPage);
+    }
 }
--- src/main/java/com/project/cheerha/domain/data/service/DataService.java
+++ src/main/java/com/project/cheerha/domain/data/service/DataService.java
@@ -1,10 +1,38 @@
 package com.project.cheerha.domain.data.service;
 
+import com.project.cheerha.domain.data.dto.response.ReadDataResponseDto;
+import com.project.cheerha.domain.data.entity.Data;
+import com.project.cheerha.domain.data.repository.DataRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class DataService {
 
+    private final DataRepository dataRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ReadDataResponseDto> readData(
+            String education, LocalDate hiringStartPeriod,
+            LocalDate hiringEndPeriod,
+            String location, int career,
+            String jobType, String requiredSkill,
+            Pageable pageable
+    ) {
+        Page<Data> dataPage = dataRepository.findAllByCondition(
+                education, hiringStartPeriod,
+                hiringEndPeriod, location, career,
+                jobType, requiredSkill,
+                pageable);
+
+        Page<ReadDataResponseDto> dtoPage = dataPage.map(ReadDataResponseDto::toDto);
+
+        return dtoPage;
+    }
 }
