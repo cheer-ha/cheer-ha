63d1418973b8211d7e98ce15847909455596c90c
“llRosell” <llovein0920@gmail.com>
2025-02-12 20:43:55 +0900

    🐛 fix : 리다이렉트 충돌 해결

--- src/main/java/com/project/cheerha/domain/data/service/DataService.java
+++ src/main/java/com/project/cheerha/domain/data/service/DataService.java
@@ -1,9 +1,12 @@
 package com.project.cheerha.domain.data.service;
 
+import com.project.cheerha.common.exception.BaseException;
 import com.project.cheerha.common.exception.CustomException;
 import com.project.cheerha.common.exception.ErrorCode;
 import com.project.cheerha.domain.data.entity.Data;
 import com.project.cheerha.domain.data.repository.DataRepository;
+import java.util.Base64;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,14 +15,17 @@ import org.springframework.stereotype.Service;
 public class DataService {
 
     private final DataRepository dataRepository;
+
     public String urlFindById(Long id) {
         Data data = dataRepository.findById(id).orElseThrow(
-                () -> new CustomException(ErrorCode.URL_NOT_FOUND)
+            () -> new CustomException(ErrorCode.URL_NOT_FOUND)
         );
         String url = data.getUrl();
+
         if (!url.startsWith("http")) {
             url = "https://" + url;
         }
+
         System.out.println("🔍 최종 리다이렉트 URL: " + url);
         data.upCnt(); //todo:아직 혹시모를 cnt 관련 예외처리 하나도 안 했음.
         dataRepository.save(data);
