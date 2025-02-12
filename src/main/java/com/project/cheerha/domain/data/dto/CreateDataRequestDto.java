bc8fa4ecc5d3725c1ea0b27861903fff1a7691e2
“llRosell” <llovein0920@gmail.com>
2025-02-12 19:50:07 +0900

    ♻️ refactor : 사용하지 않는 메서드 삭제

--- src/main/java/com/project/cheerha/domain/data/dto/CreateDataRequestDto.java
+++ src/main/java/com/project/cheerha/domain/data/dto/CreateDataRequestDto.java
@@ -1,10 +1,7 @@
 package com.project.cheerha.domain.data.dto;
 
-import com.project.cheerha.domain.data.entity.Data;
-import com.project.cheerha.domain.keyword.entity.DataKeyword;
-
 import java.time.LocalDate;
-import java.util.Set;
+import java.util.List;
 
 public record CreateDataRequestDto(
         String title,
@@ -18,41 +15,7 @@ public record CreateDataRequestDto(
         String education,
         Integer career,
         String position,
-        Set<String> requiredSkills // 추가된 필드: 자격 요건
+        List<String> requiredSkillList
 ) {
-    // CreateDataRequestDto에서 Data 엔티티를 생성하는 팩토리 메서드
-    public Data toEntity(Set<DataKeyword> dataKeywords) {
-        return new Data(
-                title,
-                company,
-                location,
-                salary,
-                jobType,
-                url,
-                hiringStartPeriod,
-                hiringEndPeriod,
-                education,
-                career,
-                position,
-                dataKeywords
-        );
-    }
 
-    // DTO에서 Data 엔티티로 변환하는 팩토리 메서드
-    public static Data fromDto(CreateDataRequestDto dto, Set<DataKeyword> dataKeywords) {
-        return new Data(
-                dto.title(),  // getter 없이 바로 필드 이름 사용
-                dto.company(),
-                dto.location(),
-                dto.salary(),
-                dto.jobType(),
-                dto.url(),
-                dto.hiringStartPeriod(),
-                dto.hiringEndPeriod(),
-                dto.education(),
-                dto.career(),
-                dto.position(),
-                dataKeywords // DataKeyword와의 연결
-        );
-    }
 }
