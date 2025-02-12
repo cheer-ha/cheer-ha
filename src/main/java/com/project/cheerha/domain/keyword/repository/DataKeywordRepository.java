36e08749a91af4f28494deb81e57f5d50f147fb8
“llRosell” <llovein0920@gmail.com>
2025-02-12 20:43:25 +0900

    🔥 delete : 패키지명 수정으로 인해 수정전 파일 삭제

--- src/main/java/com/project/cheerha/domain/keyword/repository/DataKeywordRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.cheerha.domain.keyword.repository;
-
-import com.project.cheerha.domain.keyword.entity.DataKeyword;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataKeywordRepository extends JpaRepository<DataKeyword, Long> {
-
-}
