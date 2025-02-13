649948ae523cf54c24221f2e6dd242455c1e9c29
“llRosell” <llovein0920@gmail.com>
2025-02-13 12:33:22 +0900

    ♻️ refactor : 북마크 존재여부 existsByUserIdAndJobOpeningId 조회로 수정

--- src/main/java/com/project/cheerha/domain/bookmark/repository/BookmarkRepository.java
+++ src/main/java/com/project/cheerha/domain/bookmark/repository/BookmarkRepository.java
@@ -5,14 +5,12 @@ import com.project.cheerha.domain.jobOpening.entity.JobOpening;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     // userId와 jobOpeningId로 북마크 조회
-    Optional<Bookmark> findByUserIdAndJobOpeningId(Long userId, Long jobOpeningId);
+    boolean existsByUserIdAndJobOpeningId(Long userId, Long jobOpeningId);
 
     // userId를 기준으로 북마크 조회 (Pageable을 사용한 페이징 처리)
     Page<Bookmark> findByUserId(Long userId, Pageable pageable);
