f9c26462d4664852990e0d7887ccaea7df72b111
tmdcksdl <tmdcksdl99@naver.com>
2025-02-13 19:37:42 +0900

    ✨ feat: 최근 검색 기록 조회 구현

--- src/main/java/com/project/cheerha/domain/history/controller/HistoryController.java
+++ src/main/java/com/project/cheerha/domain/history/controller/HistoryController.java
@@ -1,11 +1,33 @@
 package com.project.cheerha.domain.history.controller;
 
+import com.project.cheerha.common.annotation.Auth;
+import com.project.cheerha.common.dto.AuthUser;
+import com.project.cheerha.domain.history.dto.response.ReadHistoryResponseDto;
+import com.project.cheerha.domain.history.service.HistoryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+import java.util.List;
+
+@RequestMapping("/search/history")
 @RestController
 @RequiredArgsConstructor
 public class HistoryController {
+
+    private final HistoryService historyService;
+
+    @GetMapping
+    public ResponseEntity<List<ReadHistoryResponseDto>> readAllHistories(
+        @Auth AuthUser authUser
+    ) {
+        Long userId = authUser.id();
+
+        List<ReadHistoryResponseDto> dtoList = historyService.readAllHistories(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(dtoList);
+    }
 }
--- src/main/java/com/project/cheerha/domain/history/service/HistoryService.java
+++ src/main/java/com/project/cheerha/domain/history/service/HistoryService.java
@@ -1,10 +1,26 @@
 package com.project.cheerha.domain.history.service;
 
+import com.project.cheerha.domain.history.dto.response.ReadHistoryResponseDto;
+import com.project.cheerha.domain.history.entity.History;
+import com.project.cheerha.domain.history.repository.HistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class HistoryService {
+
+    private final HistoryRepository historyRepository;
+
+    public List<ReadHistoryResponseDto> readAllHistories(Long userId) {
+        List<History> historyList = historyRepository.findAllByUserIdOrderByCreatedAtDesc(userId);
+
+        return historyList.stream()
+            .map(ReadHistoryResponseDto::toDto)
+            .collect(Collectors.toList());
+    }
 
 }
