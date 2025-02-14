f4092761de330989e993f3e55ee39699576410af
lcy <plmokn48@daum.net>
2025-02-14 13:38:45 +0900

    📝chore: 오타 수정

--- src/main/java/com/project/cheerha/common/dto/AuthUser.java
+++ src/main/java/com/project/cheerha/common/dto/AuthUser.java
@@ -4,7 +4,7 @@ import com.project.cheerha.domain.user.entity.User.Role;
 
 /**
  * 현재 사용자의 정보를 담는 DTO
- * @param id 현재 사용자의 식별자 식별자
+ * @param id 현재 사용자의 식별자
  * @param role 현재 사용자의 권한정보
  */
 public record AuthUser(Long id, Role role) {
