53419de329f97880c9624067cf60233ba4c5409e
tmdcksdl <tmdcksdl99@naver.com>
2025-02-14 13:43:31 +0900

    ♻️ refactor: UserFindByService 사용
    - UserFindByService의 findById 메서드로 수정

--- src/main/java/com/project/cheerha/domain/user/service/UserService.java
+++ src/main/java/com/project/cheerha/domain/user/service/UserService.java
@@ -1,11 +1,8 @@
 package com.project.cheerha.domain.user.service;
 
 import com.project.cheerha.common.dto.AuthUser;
-import com.project.cheerha.common.exception.data.DataErrorCode;
-import com.project.cheerha.common.exception.data.NotFoundException;
 import com.project.cheerha.domain.user.dto.response.ReadUserResponseDto;
 import com.project.cheerha.domain.user.entity.User;
-import com.project.cheerha.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,11 +10,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
+    private final UserFindByService userFindByService;
 
     public ReadUserResponseDto readUser(AuthUser authUser) {
-        User user = userRepository.findById(authUser.id())
-            .orElseThrow(() -> new NotFoundException(DataErrorCode.USER_NOT_FOUND));
+        User user = userFindByService.findById(authUser.id());
+
         return ReadUserResponseDto.of(user.getEmail(), user.getName(), user.getCareer());
     }
 }
