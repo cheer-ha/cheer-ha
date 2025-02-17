be07c60665282d8d538593ff4f45f94ed0f0ca84
backswan0 <rnrwprh3434@gmail.com>
2025-02-17 11:41:00 +0900

    :recycle: refactor: 알림 생성에 필요한 DTO 수정
    - 데이터베이스 조회 횟수를 줄이고자 DTO 4개를 두 개씩 묶어서 총 2개로 줄임

--- src/main/java/com/project/cheerha/domain/notice/dto/JobOpeningKeywordDto.java
+++ src/main/java/com/project/cheerha/domain/notice/dto/JobOpeningKeywordDto.java
@@ -2,7 +2,11 @@ package com.project.cheerha.domain.notice.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 
-public record JobOpeningKeywordDto(Long jobOpeningId, Long keywordId) {
+public record JobOpeningKeywordDto(
+    Long jobOpeningId,
+    Long keywordId,
+    String url
+) {
 
     @QueryProjection
     public JobOpeningKeywordDto {
--- src/main/java/com/project/cheerha/domain/notice/dto/UserKeywordDto.java
+++ src/main/java/com/project/cheerha/domain/notice/dto/UserKeywordDto.java
@@ -2,7 +2,11 @@ package com.project.cheerha.domain.notice.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 
-public record UserKeywordDto(Long userId, Long keywordId) {
+public record UserKeywordDto(
+    Long userId,
+    Long keywordId,
+    String email
+) {
 
     @QueryProjection
     public UserKeywordDto {
