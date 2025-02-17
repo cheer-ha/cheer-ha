1ae7f7c326cbafb634f55fe9e9cbbb012c446af6
tmdcksdl <tmdcksdl99@naver.com>
2025-02-18 00:28:12 +0900

    ✨ feat: Enum 타입 설정
    - 학력, 고용 형태 타입을 Enum으로 변경하기 위해 Enum에 값 추가
    - String 값을 Enum 형태로 바꿔주는 메서드 추가

--- src/main/java/com/project/cheerha/domain/jobOpening/entity/EducationLevel.java
+++ src/main/java/com/project/cheerha/domain/jobOpening/entity/EducationLevel.java
@@ -1,3 +1,18 @@
 package com.project.cheerha.domain.jobOpening.entity;
 
-public enum EducationLevel {}
+import com.project.cheerha.common.exception.client.BadRequestException;
+import com.project.cheerha.common.exception.client.ClientErrorCode;
+
+import java.util.Arrays;
+
+public enum EducationLevel {
+
+    정규직, 계약직, 아르바이트, 인턴, 프리랜서;
+
+    public static EducationLevel toEnum(String educationLevel) {
+        return Arrays.stream(EducationLevel.values())
+            .filter(r -> r.name().equalsIgnoreCase(educationLevel))
+            .findFirst()
+            .orElseThrow(() -> new BadRequestException(ClientErrorCode.INVALID_ENUM_VALUE));
+    }
+}
--- src/main/java/com/project/cheerha/domain/jobOpening/entity/EmploymentType.java
+++ src/main/java/com/project/cheerha/domain/jobOpening/entity/EmploymentType.java
@@ -1,3 +1,18 @@
 package com.project.cheerha.domain.jobOpening.entity;
 
-public enum EmploymentType {}
+import com.project.cheerha.common.exception.client.BadRequestException;
+import com.project.cheerha.common.exception.client.ClientErrorCode;
+
+import java.util.Arrays;
+
+public enum EmploymentType {
+
+    무관, 고졸, 전문학사, 학사, 석사, 박사;
+
+    public static EmploymentType toEnum(String employmentType) {
+        return Arrays.stream(EmploymentType.values())
+            .filter(r -> r.name().equalsIgnoreCase(employmentType))
+            .findFirst()
+            .orElseThrow(() -> new BadRequestException(ClientErrorCode.INVALID_ENUM_VALUE));
+    }
+}
