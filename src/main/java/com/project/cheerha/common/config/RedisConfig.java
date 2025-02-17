c9d171fa962261707986d746e85a6922a8369f35
joyangjin <wndid2008@gmail.com>
2025-02-17 23:46:48 +0900

    :recycle: add RedisConfig

---
+++ src/main/java/com/project/cheerha/common/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.project.cheerha.common.config;
+
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.setCodec(new JsonJacksonCodec());
+        config.useSingleServer()
+               .setAddress("redis://redis:6379");  //도커로 실행할 경우
+//                .setAddress("redis://localhost:6379");    //로컬에서 실행할 경우
+        return org.redisson.Redisson.create(config);
+    }
+}
