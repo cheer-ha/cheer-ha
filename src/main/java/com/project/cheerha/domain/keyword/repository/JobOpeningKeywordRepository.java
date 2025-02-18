ddf7e50c8c9debe41026e3b389d91b52dec12a07
backswan0 <rnrwprh3434@gmail.com>
2025-02-18 19:04:19 +0900

    :memo: chore: 불필요한 메서드 삭제
    - QueryDSL로 대체된 메서드 삭제

--- src/main/java/com/project/cheerha/domain/keyword/repository/JobOpeningKeywordRepository.java
+++ src/main/java/com/project/cheerha/domain/keyword/repository/JobOpeningKeywordRepository.java
@@ -1,20 +1,8 @@
 package com.project.cheerha.domain.keyword.repository;
 
 import com.project.cheerha.domain.keyword.entity.JobOpeningKeyword;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface JobOpeningKeywordRepository extends JpaRepository<JobOpeningKeyword, Long> {
 
-    // [수정 후 Query]
-    @Query(
-        "SELECT DISTINCT jok.jobOpening.id "
-            + "FROM JobOpeningKeyword jok "
-            + "WHERE jok.keyword.id IN :keywordIdList"
-    )
-    List<Long> findJobOpeningIdListByKeywordId(
-        @Param("keywordIdList") List<Long> keywordIdList
-    );
 }
