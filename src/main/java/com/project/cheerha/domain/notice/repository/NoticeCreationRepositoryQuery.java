3d008f68b8b7bb59d23fb24fd9899d35d3d2311d
backswan0 <rnrwprh3434@gmail.com>
2025-02-18 19:15:36 +0900

    :recycle: refactor: Projections 대신 생성자 사용
    - DTO 생성 시 생성자 사용
    
    - QueryDSL 또한 생성자 사용으로 리팩토링
    
    - 주석 추가

--- src/main/java/com/project/cheerha/domain/notice/repository/NoticeCreationRepositoryQuery.java
+++ src/main/java/com/project/cheerha/domain/notice/repository/NoticeCreationRepositoryQuery.java
@@ -7,7 +7,9 @@ import java.util.List;
 
 public interface NoticeCreationRepositoryQuery {
 
+    // 데이터베이스에서 사용자 관련 정보를 조회해오는 메서드
     List<UserKeywordDto> findAllUserKeywords();
 
+    // 데이터베이스에서 채용 공고 관련 정보를 조회해오는 메서드
     List<JobOpeningKeywordDto> findAllJobOpeningKeywords(ZonedDateTime referenceTime);
 }
--- src/main/java/com/project/cheerha/domain/notice/repository/NoticeCreationRepositoryQueryImpl.java
+++ src/main/java/com/project/cheerha/domain/notice/repository/NoticeCreationRepositoryQueryImpl.java
@@ -4,9 +4,10 @@ import com.project.cheerha.domain.jobOpening.entity.QJobOpening;
 import com.project.cheerha.domain.keyword.entity.QJobOpeningKeyword;
 import com.project.cheerha.domain.keyword.entity.QUserKeyword;
 import com.project.cheerha.domain.notice.dto.JobOpeningKeywordDto;
+import com.project.cheerha.domain.notice.dto.QJobOpeningKeywordDto;
+import com.project.cheerha.domain.notice.dto.QUserKeywordDto;
 import com.project.cheerha.domain.notice.dto.UserKeywordDto;
 import com.project.cheerha.domain.user.entity.QUser;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.time.ZonedDateTime;
 import java.util.List;
@@ -19,31 +20,31 @@ public class NoticeCreationRepositoryQueryImpl implements NoticeCreationReposito
 
     private final JPAQueryFactory queryFactory;
 
+    // 데이터베이스에서 사용자 관련 정보를 조회해오는 메서드
     @Override
     public List<UserKeywordDto> findAllUserKeywords() {
         QUserKeyword userKeyword = QUserKeyword.userKeyword;
         QUser user = QUser.user;
 
         return queryFactory
-            .select(Projections.constructor(
-                    UserKeywordDto.class,
+            .select(
+                new QUserKeywordDto(
                     userKeyword.user.id,
                     userKeyword.keyword.id,
                     user.email
                 )
-            )
-            .from(userKeyword)
+            ).from(userKeyword)
             .join(userKeyword.user, user)
             .fetch();
     }
 
+    // 데이터베이스에서 채용 공고 관련 정보를 조회해오는 메서드
     public List<JobOpeningKeywordDto> findAllJobOpeningKeywords(ZonedDateTime referenceTime) {
         QJobOpeningKeyword jobOpeningKeyword = QJobOpeningKeyword.jobOpeningKeyword;
         QJobOpening jobOpening = QJobOpening.jobOpening;
 
         return queryFactory
-            .select(Projections.constructor(
-                    JobOpeningKeywordDto.class,
+            .select(new QJobOpeningKeywordDto(
                     jobOpeningKeyword.jobOpening.id,
                     jobOpeningKeyword.keyword.id,
                     jobOpening.jobOpeningUrl
