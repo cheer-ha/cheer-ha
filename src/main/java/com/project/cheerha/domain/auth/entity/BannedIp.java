090c04516732ea76707177fcb7b98062046fc627
lcy <plmokn48@daum.net>
2025-02-20 07:56:23 +0900

    🎉 add: 밴 ip, email 엔티티 추가

---
+++ src/main/java/com/project/cheerha/domain/auth/entity/BannedIp.java
@@ -0,0 +1,25 @@
+package com.project.cheerha.domain.auth.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "banned_ip")
+public class BannedIp {
+
+    @Id
+    private String ip;
+
+    private String message;
+
+    private ZonedDateTime createdAt = ZonedDateTime.now();
+
+    private boolean isDeleted = false;
+}
