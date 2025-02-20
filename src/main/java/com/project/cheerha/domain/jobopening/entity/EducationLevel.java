d6f6655f9fa8b7bd97deb499a47d1ed00d977be3
“llRosell” <llovein0920@gmail.com>
2025-02-20 19:42:44 +0900

    🙈🐛 fix: 병합 충돌 해결

---
+++ src/main/java/com/project/cheerha/domain/jobopening/entity/EducationLevel.java
@@ -0,0 +1,19 @@
+package com.project.cheerha.domain.jobopening.entity;
+
+import com.project.cheerha.common.exception.client.BadRequestException;
+import com.project.cheerha.common.exception.client.ClientErrorCode;
+
+import java.util.Arrays;
+
+public enum EducationLevel {
+
+    무관, 고졸, 대졸, 학사, 석사, 박사;
+
+    public static EducationLevel toEnum(String educationLevel) {
+        return Arrays.stream(EducationLevel.values())
+                .filter(r -> r.name().equalsIgnoreCase(educationLevel))
+                .findFirst()
+                .orElseThrow(() -> new BadRequestException(ClientErrorCode.INVALID_ENUM_VALUE));
+    }
+}
+
