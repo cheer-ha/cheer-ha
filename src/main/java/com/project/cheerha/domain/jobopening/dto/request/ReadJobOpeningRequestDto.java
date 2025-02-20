fabe78700bd709de642984c68b6a89387a091da1
joyangjin <wndid2008@gmail.com>
2025-02-20 13:03:07 +0900

    Fix case-sensitive file path for JobOpeningRepository.java

---
+++ src/main/java/com/project/cheerha/domain/jobopening/dto/request/ReadJobOpeningRequestDto.java
@@ -0,0 +1,28 @@
+package com.project.cheerha.domain.jobopening.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReadJobOpeningRequestDto {
+
+    private String educationLevel;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private ZonedDateTime hiringStartAt;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private ZonedDateTime hiringEndAt;
+
+    private String location;
+    private Integer experienceYears;
+    private String employmentType;
+    private String requiredSkill;
+
+    private String searchTerm;
+}
