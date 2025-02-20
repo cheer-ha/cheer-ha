960ce244d99a7b7728aaaccc41daceddacd9090b
lcy <plmokn48@daum.net>
2025-02-20 18:01:02 +0900

    🐛 fix: user에 enum타입 string으로 명시 안했던 오류 수정

--- src/main/java/com/project/cheerha/domain/user/entity/User.java
+++ src/main/java/com/project/cheerha/domain/user/entity/User.java
@@ -1,18 +1,13 @@
 package com.project.cheerha.domain.user.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "`user`") //user가 예약어라서 이 부분 ``로 안감싸면 오류나요.
+@Table(name = "`user`")
 public class User {
 
     @Id
@@ -34,6 +29,7 @@ public class User {
     @Column(length = 255, nullable = false )
     private String password;
 
+    @Enumerated(EnumType.STRING)
     @Column(length = 5, nullable = false )
     private Role role;
 
