bb082496e5b049cca5282737bdcbed3ee3be19b4
lcy <plmokn48@daum.net>
2025-02-22 19:11:24 +0900

    🔥 delete: 기존 코드로 롤백

--- src/main/java/com/project/cheerha/domain/jobopening/service/JobOpeningInsertScheduler.java
+++ src/main/java/com/project/cheerha/domain/jobopening/service/JobOpeningInsertScheduler.java
@@ -1,8 +1,6 @@
 package com.project.cheerha.domain.jobopening.service;
 
 import com.project.cheerha.common.annotation.ScheduledDynamic;
-import com.project.cheerha.common.exception.data.ConflictException;
-import com.project.cheerha.common.exception.data.DataErrorCode;
 import com.project.cheerha.domain.jobopening.entity.JobOpening;
 import com.project.cheerha.domain.jobopening.repository.JobOpeningRepository;
 import com.project.cheerha.domain.keyword.entity.JobOpeningKeyword;
@@ -22,12 +20,10 @@ public class JobOpeningInsertScheduler {
     private final Random random = new Random();
 
     private static final int JOB_COUNT = 3; //한 번에 생성할 최대 jobOpening 개수
-    private static final String LOCK_PREFIX = "job_";
 
     /**
      * minMinutes - maxMinutes 사이의 랜덤한 시간 간격을 두고 랜덤한 채용공고를 삽입합니다.
      * 한 번에 최소 1 - 최대 JOB_COUNT 개의 데이터를 생성합니다.
-     * 네임드락을 걸어 스케줄러의 중복 실행을 방지합니다.
      */
     @ScheduledDynamic(minMinutes = 1, maxMinutes = 2)
     public void insertRandomJobOpening() {
@@ -40,10 +36,6 @@ public class JobOpeningInsertScheduler {
         List<JobOpeningKeyword> allJobOpeningKeywords = new ArrayList<>();
 
         for (int i = 0; i < jobCount; i++) {
-            if (!Objects.equals(jobOpeningRepository.getLock(LOCK_PREFIX + i), 1)) { //락 획득 실패 시
-                log.warn("다른 스케줄러가 이미 실행 중이므로 종료.");
-                throw new ConflictException(DataErrorCode.NAMED_LOCK_CONFLICT);
-            }
             JobOpening jobOpening = JobOpeningFactory.createRandomJobOpening();
             jobOpenings.add(jobOpening);
 
