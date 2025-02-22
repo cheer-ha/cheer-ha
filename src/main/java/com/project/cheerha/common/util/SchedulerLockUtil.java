65d135cfcc0651d12baa9f1f72007945290932e7
lcy <plmokn48@daum.net>
2025-02-22 21:24:26 +0900

    ♻️ refactor: 다른 스케줄러에서도 사용할 수 있도록 리팩토링, 스케줄러의 과한 책임분리

---
+++ src/main/java/com/project/cheerha/common/util/SchedulerLockUtil.java
@@ -0,0 +1,43 @@
+package com.project.cheerha.common.util;
+
+import com.project.cheerha.common.exception.server.IllegalStatusException;
+import com.project.cheerha.common.exception.server.ServerErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class SchedulerLockUtil {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    private static final String LOCK_VALUE = InstanceUtil.getInstanceId();
+
+    /**
+     * 인스턴스 단위로 스케줄링을 관리하는 유틸메서드입니다.
+     * key 와 ttl 을 인자로 받아 redis 에서 관리합니다.
+     */
+    public void lock(String keyName, long ttl) {
+        //setIfAbsent 로 원자적으로 락을 잡으려 시도
+        Boolean acquired = redisTemplate
+                .opsForValue()
+                .setIfAbsent(keyName, LOCK_VALUE, ttl, TimeUnit.MINUTES);
+
+        //락을 못 잡았다면 내 인스턴스의 락인지 확인
+        if (Boolean.FALSE.equals(acquired)) {
+            String currentValue = redisTemplate.opsForValue().get(keyName);
+            //다른 인스턴스의 소유라면 그냥 리턴
+            if (!LOCK_VALUE.equals(currentValue)) {
+                log.info("다른 인스턴스에서 실행중인 스케줄러입니다.");
+                throw new IllegalStatusException(ServerErrorCode.ALREADY_RUNNING_SCHEDULER);
+            }
+            //만약 내 인스턴스라면 이전 스케줄이 남긴 락이거나 ttl 이 안 끝난 상황일 수 있으므로 ttl 재갱신
+            redisTemplate.expire(keyName, ttl, TimeUnit.MINUTES);
+        }
+    }
+}
--- src/main/java/com/project/cheerha/domain/jobopening/service/JobOpeningInsertScheduler.java
+++ src/main/java/com/project/cheerha/domain/jobopening/service/JobOpeningInsertScheduler.java
@@ -1,17 +1,16 @@
 package com.project.cheerha.domain.jobopening.service;
 
 import com.project.cheerha.common.annotation.ScheduledDynamic;
-import com.project.cheerha.common.util.InstanceUtil;
+import com.project.cheerha.common.exception.server.IllegalStatusException;
+import com.project.cheerha.common.util.SchedulerLockUtil;
 import com.project.cheerha.domain.jobopening.entity.JobOpening;
 import com.project.cheerha.domain.jobopening.repository.JobOpeningRepository;
 import com.project.cheerha.domain.keyword.entity.JobOpeningKeyword;
 import com.project.cheerha.domain.keyword.repository.JobOpeningKeywordRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
@@ -20,57 +19,47 @@ public class JobOpeningInsertScheduler {
 
     private final JobOpeningRepository jobOpeningRepository;
     private final JobOpeningKeywordRepository jobOpeningKeywordRepository;
-    private final RedisTemplate<String, String> redisTemplate;
+    private final SchedulerLockUtil schedulerLockUtil;
     private final Random random = new Random();
 
     private static final int JOB_COUNT = 3; //한 번에 생성할 최대 jobOpening 개수
     private static final String LOCK_KEY = "insert_job_opening_lock";
-    private static final String LOCK_VALUE = InstanceUtil.getInstanceId();
-    private static final long LOCK_TTL = 3; //3분간 유지
+    private static final long LOCK_TTL = 4; //3분간 유지(minMinutes + 1)
 
     /**
      * minMinutes - maxMinutes 사이의 랜덤한 시간 간격을 두고 랜덤한 채용공고를 삽입합니다.
      * 한 번에 최소 1 - 최대 JOB_COUNT 개의 데이터를 생성합니다.
      */
-    @ScheduledDynamic(minMinutes = 1, maxMinutes = 2)
+    @ScheduledDynamic(minMinutes = 3, maxMinutes = 40)
     public void insertRandomJobOpening() {
-        //setIfAbsent 로 원자적으로 락을 잡으려 시도
-        Boolean acquired = redisTemplate
-                .opsForValue()
-                .setIfAbsent(LOCK_KEY, LOCK_VALUE, LOCK_TTL, TimeUnit.MINUTES);
+        try {
+            schedulerLockUtil.lock(LOCK_KEY, LOCK_TTL);
 
-        //락을 못 잡았다면 내 인스턴스의 락인지 확인
-        if (Boolean.FALSE.equals(acquired)) {
-            String currentValue = redisTemplate.opsForValue().get(LOCK_KEY);
-            //다른 인스턴스의 소유라면 그냥 리턴
-            if (!LOCK_VALUE.equals(currentValue)) {
-                log.info("다른 인스턴스에서 실행중인 스케줄러입니다.");
-                return;
+            log.info("랜덤 채용공고 데이터 삽입 시작");
+
+            int jobCount = random.nextInt(JOB_COUNT) + 1;
+            log.info("{}개의 채용 공고 삽입 예정", jobCount);
+
+            List<JobOpening> jobOpenings = new ArrayList<>();
+            List<JobOpeningKeyword> allJobOpeningKeywords = new ArrayList<>();
+
+            for (int i = 0; i < jobCount; i++) {
+                JobOpening jobOpening = JobOpeningFactory.createRandomJobOpening();
+                jobOpenings.add(jobOpening);
+
+                List<JobOpeningKeyword> jobOpeningKeywords =
+                        JobOpeningKeywordFactory.createRandomKeywordList(jobOpening);
+                allJobOpeningKeywords.addAll(jobOpeningKeywords);
             }
-            //만약 내 인스턴스라면 이전 스케줄이 남긴 락이거나
-            //ttl 이 안 끝난 상황일 수 있음. ttl 재갱신
-            redisTemplate.expire(LOCK_KEY, LOCK_TTL, TimeUnit.MINUTES);
+
+            jobOpeningRepository.saveAll(jobOpenings);
+            jobOpeningKeywordRepository.saveAll(allJobOpeningKeywords);
+
+            log.info("총 {}개의 채용 공고가 삽입되었습니다.", jobOpenings.size());
+        } catch (IllegalStatusException e) {
+            log.warn("현재 인스턴스의 스케줄러를 종료합니다.");
+        } catch (Exception e) {
+            log.error("채용공고 삽입 중 예상치 못한 예외 발생", e);
         }
-
-        log.info("랜덤 채용공고 데이터 삽입 시작");
-
-        int jobCount = random.nextInt(JOB_COUNT) + 1;
-        log.info("{}개의 채용 공고 삽입 예정", jobCount);
-
-        List<JobOpening> jobOpenings = new ArrayList<>();
-        List<JobOpeningKeyword> allJobOpeningKeywords = new ArrayList<>();
-
-        for (int i = 0; i < jobCount; i++) {
-            JobOpening jobOpening = JobOpeningFactory.createRandomJobOpening();
-            jobOpenings.add(jobOpening);
-
-            List<JobOpeningKeyword> jobOpeningKeywords = JobOpeningKeywordFactory.createRandomKeywordList(jobOpening);
-            allJobOpeningKeywords.addAll(jobOpeningKeywords);
-        }
-
-        jobOpeningRepository.saveAll(jobOpenings);
-        jobOpeningKeywordRepository.saveAll(allJobOpeningKeywords);
-
-        log.info("총 {}개의 채용 공고가 삽입되었습니다.", jobOpenings.size());
     }
 }
