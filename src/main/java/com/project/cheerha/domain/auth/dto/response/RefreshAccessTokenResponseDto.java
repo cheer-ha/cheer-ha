eb0fdc38c70564909b404f064a2ca967cdc48ef9
lcy <plmokn48@daum.net>
2025-02-22 22:06:21 +0900

    🐛 fix: refreshAccessToken에서 새로운 refreshToken을 반환하지 않던 오류 수정

--- src/main/java/com/project/cheerha/domain/auth/dto/response/RefreshAccessTokenResponseDto.java
+++ src/main/java/com/project/cheerha/domain/auth/dto/response/RefreshAccessTokenResponseDto.java
@@ -1,9 +1,9 @@
 package com.project.cheerha.domain.auth.dto.response;
 
 public record RefreshAccessTokenResponseDto(
-        String accessToken, String message
+        String accessToken, String message, String refreshToken
 ) {
-    public static RefreshAccessTokenResponseDto of(String accessToken) {
-        return new RefreshAccessTokenResponseDto(accessToken, "AccessToken 재발급 성공");
+    public static RefreshAccessTokenResponseDto of(String accessToken, String refreshToken) {
+        return new RefreshAccessTokenResponseDto(accessToken, "AccessToken 재발급 성공", refreshToken);
     }
 }
