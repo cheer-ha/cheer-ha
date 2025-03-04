80283822644d5ba5b6a1c747431e49d9e56d93f6
backswan0 <rnrwprh3434@gmail.com>
2025-03-05 03:16:43 +0900

    :recycle: refactor: 알림 생성 서비스 메서드 분리
    - 특수 로직인 만큼 별도의 클래스 생성 대신 private 메서드로 추출
    
    - [할 일] 줄 바꿈 및 주석 작성

--- src/main/java/com/project/cheerha/domain/notice/service/NotificationService.java
+++ src/main/java/com/project/cheerha/domain/notice/service/NotificationService.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-// 사용자와 채용 공고 URL을 연결하는 Service
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
@@ -21,63 +20,45 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
 
     @Transactional
-    public void saveMappings(
-        List<UserDto> userDtoList,
-        Map<Long, List<String>> keywordIdToUrlList
-    ) {
-
-        // todo 메서드 단위로 나누기 private으로 추출한 번 해보기
-        // todo 지금은 구현체를 쭉 읽어야 한다면, 추출하면 메서드를 읽으면서 흐름 파악 가능 -> 그리고 구현체가 궁금하면 맨 밑에 찾아볼 것임
-        // todo 특수 로직이므로 다른 클래스로 뺄 필요는 없음
-
-        // key: 사용자 이메일
-        // value: 해당 이메일에 연결되는 채용 공고 URL 목록
+    public void createNotification(List<UserDto> userDtoList,
+        Map<Long, List<String>> keywordIdToUrlList) {
         Map<String, Set<String>> emailToUrl = new HashMap<>();
 
-        // 사용자 정보를 순회하며 이메일별로 URL 목록 구성
-        for (UserDto dto : userDtoList) {
-            List<String> matchingUrlList = keywordIdToUrlList
-                .getOrDefault(
-                    dto.keywordId(),
-                    List.of()
-                );
+        mapEmailToUrls(userDtoList, keywordIdToUrlList, emailToUrl);
 
-            // 기존에 존재하지 않으면 새로운 HashSet 생성 후 추가
-            if (!matchingUrlList.isEmpty()) {
-                emailToUrl.computeIfAbsent(
-                    dto.email(),
-                    email -> new HashSet<>()
-                ).addAll(matchingUrlList);
-            }
-        }
-
-        // 이메일별로 연결된 채용 공고 URL 목록 처리
         emailToUrl.forEach((email, urlSet) -> {
 
-            // 기존에 저장된 Mapping 목록 조회
-            List<Notification> foundNotificationList = notificationRepository.findAllByEmailAndJobOpeningUrlIn(
-                email,
-                urlSet.stream().toList()
-            );
+            List<Notification> foundNotificationList = notificationRepository.findAllByEmailAndJobOpeningUrlIn(email, urlSet.stream().toList());
 
-            // 이미 존재하는 Mapping의 URL 목록을 Set으로 변환
-            // 중복을 확인할 때 사용
-            Set<String> existingUrlSet = foundNotificationList.stream()
-                .map(Notification::getJobOpeningUrl)
-                .collect(Collectors.toSet());
+            Set<String> existingUrlSet = findExistingUrlSet(foundNotificationList);
 
-            // 중복 확인 후 기존에 없는 Mapping 객체만 생성
-            // .filter : 중복 제거
-            // .map: Mapping 객체 생성
-            List<Notification> notificationList = urlSet.stream()
-                .filter(jobOpeningUrl ->
-                    !existingUrlSet.contains(jobOpeningUrl)
-                ).map(jobOpeningUrl ->
-                    Notification.toEntity(email, jobOpeningUrl)
-                ).toList();
+            List<Notification> notificationList = createNotificationList(email, urlSet, existingUrlSet);
 
-            // 새로운 Mapping 객체를 한꺼번에 데이터베이스에 저장
             notificationRepository.saveAll(notificationList);
         });
+    }
+
+    private void mapEmailToUrls(List<UserDto> userDtoList,
+        Map<Long, List<String>> keywordIdToUrlList, Map<String, Set<String>> emailToUrl) {
+        for (UserDto dto : userDtoList) {
+            List<String> matchingUrlList = keywordIdToUrlList.getOrDefault(dto.keywordId(), List.of());
+
+            if (!matchingUrlList.isEmpty()) {
+                emailToUrl.computeIfAbsent(dto.email(), email -> new HashSet<>()).addAll(matchingUrlList);
+            }
+        }
+    }
+
+    private Set<String> findExistingUrlSet(List<Notification> foundNotificationList) {
+        return foundNotificationList.stream()
+            .map(Notification::getJobOpeningUrl)
+            .collect(Collectors.toSet());
+    }
+
+    private List<Notification> createNotificationList(String email, Set<String> urlSet,
+        Set<String> existingUrlSet) {
+        return urlSet.stream()
+            .filter(jobOpeningUrl -> !existingUrlSet.contains(jobOpeningUrl))
+            .map(jobOpeningUrl -> Notification.toEntity(email, jobOpeningUrl)).toList();
     }
 }
