b50abd26d4bfc664a1d09a189705e32c4b8357f7
“llRosell” <llovein0920@gmail.com>
2025-03-05 12:26:45 +0900

    ♻️ refactor : Elasticsearch 클라이언트 호출부 클래스 분리 - Elasticsearch 관련 쿼리 실행 로직을 `ElasticsearchClientService`로 분리 - 마감된 채용공고 필터링을 상수 `CURRENT_DATE_TIME`을 사용하여 가독성 향상

--- src/main/java/com/project/cheerha/domain/elasticsearch/service/JobOpeningDocumentService.java
+++ src/main/java/com/project/cheerha/domain/elasticsearch/service/JobOpeningDocumentService.java
@@ -1,42 +1,31 @@
 package com.project.cheerha.domain.elasticsearch.service;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.elasticsearch._types.SortOrder;
 import co.elastic.clients.elasticsearch._types.query_dsl.RangeQuery;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
-import co.elastic.clients.elasticsearch.core.SearchResponse;
-import com.project.cheerha.common.exception.data.ElasticsearchQueryException;
 import com.project.cheerha.domain.elasticsearch.IndexName;
-import com.project.cheerha.common.exception.data.DataErrorCode;
 import com.project.cheerha.domain.elasticsearch.dto.request.ReadJobOpeningElasticRequestDto;
 import com.project.cheerha.domain.elasticsearch.dto.response.ReadJobOpeningElasticResponseDto;
 import com.project.cheerha.domain.elasticsearch.entity.JobOpeningDocument;
 import com.project.cheerha.domain.elasticsearch.filter.JobOpeningDocumentFilter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
+import java.time.ZonedDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class JobOpeningDocumentService {
 
-    private final ElasticsearchClient elasticsearchClient;
+    private final ElasticsearchClientService elasticsearchClientService;
 
     /**
-     * Elasticsearch를 사용하여 전체 채용공고를 조회하는 메서드입니다.
-     *
-     * @param pageable 페이지 요청 정보 (페이지 번호, 페이지 크기 등)
-     * @return 조회된 채용공고 목록을 포함하는 DTO 페이지 객체
-     * @throws ElasticsearchQueryException Elasticsearch 쿼리 실행 실패 시 발생
+     * 전체 채용공고 조회
      */
     @Transactional(readOnly = true)
     public Page<ReadJobOpeningElasticResponseDto> readAllJobOpeningsUsingElasticsearch(Pageable pageable) {
@@ -44,26 +33,13 @@ public class JobOpeningDocumentService {
         int pageNumber = pageable.getPageNumber();
         int from = calculateFrom(pageNumber, pageSize, IndexName.MAX_JOP_OPENING_SIZE);
         SearchRequest searchRequest = buildSearchRequest(from, pageSize, null);
-        try {
-            // Elasticsearch 쿼리 실행
-            SearchResponse<JobOpeningDocument> searchResponse = elasticsearchClient.search(searchRequest, JobOpeningDocument.class);
-            List<JobOpeningDocument> jobOpeningDocumentList = searchResponse.hits().hits().stream()
-                    .map(hit -> hit.source())
-                    .collect(Collectors.toList());
-            // 반환할 DTO 리스트로 변환
-            List<ReadJobOpeningElasticResponseDto> dtoList = ReadJobOpeningElasticResponseDto.toDto(jobOpeningDocumentList);
-            return new PageImpl<>(dtoList, pageable, IndexName.MAX_JOP_OPENING_SIZE);
-        } catch (IOException e) {
-            throw new ElasticsearchQueryException(DataErrorCode.JOB_OPENING_NOT_FOUND);
-        }
+        List<JobOpeningDocument> jobOpeningDocumentList = elasticsearchClientService.fetchJobOpeningDocumentList(searchRequest);
+        List<ReadJobOpeningElasticResponseDto> dtoList = ReadJobOpeningElasticResponseDto.toDto(jobOpeningDocumentList);
+        return new PageImpl<>(dtoList, pageable, IndexName.MAX_JOP_OPENING_SIZE);
     }
 
     /**
-     * 조회수 기준으로 상위 100개 인기 채용공고를 조회하는 메서드입니다.
-     *
-     * @param pageable 페이지 요청 정보 (페이지 번호, 페이지 크기 등)
-     * @return 조회수가 많은 상위 100개의 인기 채용공고 목록을 포함하는 페이지 객체
-     * @throws ElasticsearchQueryException Elasticsearch 쿼리 실행 실패 시 발생
+     * 조회수 기준 인기 채용공고 조회
      */
     @Transactional(readOnly = true)
     public Page<ReadJobOpeningElasticResponseDto> readTop100PopularJobOpeningsUsingElasticsearch(Pageable pageable) {
@@ -71,26 +47,13 @@ public class JobOpeningDocumentService {
         int pageNumber = pageable.getPageNumber();
         int from = calculateFrom(pageNumber, pageSize, IndexName.MAX_POPULAR_SIZE);
         SearchRequest searchRequest = buildSearchRequest(from, pageSize, IndexName.VIEW_COUNT);
-        try {
-            SearchResponse<JobOpeningDocument> searchResponse = elasticsearchClient.search(searchRequest, JobOpeningDocument.class);
-            List<JobOpeningDocument> jobOpeningDocuments = searchResponse.hits().hits().stream()
-                    .map(hit -> hit.source())
-                    .collect(Collectors.toList());
-            List<ReadJobOpeningElasticResponseDto> dtoList = ReadJobOpeningElasticResponseDto.toDto(jobOpeningDocuments);
-            return new PageImpl<>(dtoList, pageable, IndexName.MAX_POPULAR_SIZE);
-        } catch (IOException e) {
-            log.error("Elasticsearch 조회 실패", e);
-            throw new ElasticsearchQueryException(DataErrorCode.JOB_OPENING_NOT_FOUND);
-        }
+        List<JobOpeningDocument> jobOpeningDocuments = elasticsearchClientService.fetchJobOpeningDocumentList(searchRequest);
+        List<ReadJobOpeningElasticResponseDto> dtoList = ReadJobOpeningElasticResponseDto.toDto(jobOpeningDocuments);
+        return new PageImpl<>(dtoList, pageable, IndexName.MAX_POPULAR_SIZE);
     }
 
     /**
-     * 사용자가 제공한 필터 조건과 검색어를 기반으로 Elasticsearch에서 채용 공고 데이터를 조회하는 메서드입니다.
-     *
-     * @param requestDto 필터링 및 검색 조건을 포함한 요청 DTO
-     * @param userId 현재 로그인한 사용자의 ID
-     * @param pageable 페이지네이션 정보 (페이지 번호 및 페이지 크기)
-     * @return 필터링된 채용 공고 목록을 페이지네이션 형태로 반환
+     * 필터링된 채용공고 조회
      */
     @Transactional
     public Page<ReadJobOpeningElasticResponseDto> readJobOpeningUsingElasticSearchFilter(
@@ -110,17 +73,9 @@ public class JobOpeningDocumentService {
                 .from(from)
                 .size(pageSize)
                 .build();
-        try {
-            SearchResponse<JobOpeningDocument> searchResponse = elasticsearchClient.search(searchRequest, JobOpeningDocument.class);
-            List<JobOpeningDocument> jobOpeningDocuments = searchResponse.hits().hits().stream()
-                    .map(hit -> hit.source())
-                    .collect(Collectors.toList());
-            List<ReadJobOpeningElasticResponseDto> dtoList = ReadJobOpeningElasticResponseDto.toDto(jobOpeningDocuments);
-            return new PageImpl<>(dtoList, pageable, IndexName.MAX_JOP_OPENING_SIZE);
-        } catch (IOException e) {
-            log.error("Elasticsearch 조회 실패", e);
-            throw new ElasticsearchQueryException(DataErrorCode.JOB_OPENING_NOT_FOUND);
-        }
+        List<JobOpeningDocument> jobOpeningDocuments = elasticsearchClientService.fetchJobOpeningDocumentList(searchRequest);
+        List<ReadJobOpeningElasticResponseDto> dtoList = ReadJobOpeningElasticResponseDto.toDto(jobOpeningDocuments);
+        return new PageImpl<>(dtoList, pageable, IndexName.MAX_JOP_OPENING_SIZE);
     }
 
     /**
@@ -135,7 +90,7 @@ public class JobOpeningDocumentService {
     }
 
     /**
-     * Elasticsearch의 쿼리 요청을 설정하는 메서드
+     * Elasticsearch 쿼리 요청을 설정하는 메서드
      */
     private SearchRequest buildSearchRequest(int from, int pageSize, String sortField) {
         SearchRequest.Builder searchRequestBuilder = new SearchRequest.Builder()
@@ -158,7 +113,7 @@ public class JobOpeningDocumentService {
                 .range(RangeQuery.of(r -> r
                                 .term(t -> t
                                         .field(IndexName.HIRING_END_AT)
-                                        .gte("now")
+                                        .gte(IndexName.CURRENT_DATE_TIME)
                                 )
                         )
                 )
