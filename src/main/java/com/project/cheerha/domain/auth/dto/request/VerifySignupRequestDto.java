3a8a59f617cc047cf6690f3d39024ccc344317f8
lcy <plmokn48@daum.net>
2025-03-07 03:26:23 +0900

    📝chore: 패스워드 유효성검사 예외 message 생성

--- src/main/java/com/project/cheerha/domain/auth/dto/request/VerifySignupRequestDto.java
+++ src/main/java/com/project/cheerha/domain/auth/dto/request/VerifySignupRequestDto.java
@@ -4,8 +4,7 @@ import jakarta.validation.constraints.*;
 
 public record VerifySignupRequestDto(
     @Email String email,
-    //8글자 이상, 영소문자, 특수문자, 숫자 각 1개 이상
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).{8,}$") @NotBlank String password,
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).{8,}$", message = "올바른 비밀번호가 아닙니다.") @NotBlank String password,
     @NotBlank String name,
     @NotNull @Min(20) @Max(80) int age,
     @NotNull @Min(0) @Max(50) int career,
