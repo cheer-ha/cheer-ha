757f9248cb550e9b9da58d57de54f6bfd4345402
lcy <plmokn48@daum.net>
2025-03-07 03:21:04 +0900

    ✨ feat: 패스워드 유효성검사

--- src/main/java/com/project/cheerha/domain/auth/dto/request/VerifySignupRequestDto.java
+++ src/main/java/com/project/cheerha/domain/auth/dto/request/VerifySignupRequestDto.java
@@ -4,7 +4,8 @@ import jakarta.validation.constraints.*;
 
 public record VerifySignupRequestDto(
     @Email String email,
-    @NotBlank String password,
+    //8글자 이상, 영소문자, 특수문자, 숫자 각 1개 이상
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[\\W_]).{8,}$") @NotBlank String password,
     @NotBlank String name,
     @NotNull @Min(20) @Max(80) int age,
     @NotNull @Min(0) @Max(50) int career,
