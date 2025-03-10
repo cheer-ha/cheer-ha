6b96b90787690674fa3204e50f2febd0e15e550f
tmdcksdl <tmdcksdl99@naver.com>
2025-03-10 16:21:17 +0900

    ♻️ refactor: 연령대별 인기 키워드 조회 쿼리 최적화

--- src/main/java/com/project/cheerha/domain/keyword/repository/KeywordRepositoryQueryImpl.java
+++ src/main/java/com/project/cheerha/domain/keyword/repository/KeywordRepositoryQueryImpl.java
@@ -1,18 +1,17 @@
 package com.project.cheerha.domain.keyword.repository;
 
+import com.project.cheerha.domain.keyword.dto.response.KeywordCustomAgeResponseDto;
+import com.project.cheerha.domain.keyword.dto.response.QKeywordCustomAgeResponseDto;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 import static com.project.cheerha.domain.keyword.entity.QKeyword.keyword;
 import static com.project.cheerha.domain.user.entity.QUser.user;
 import static com.project.cheerha.domain.userkeyword.entity.QUserKeyword.userKeyword;
-import static com.querydsl.jpa.JPAExpressions.select;
-
-import com.project.cheerha.domain.keyword.dto.response.KeywordCustomAgeResponseDto;
-import com.project.cheerha.domain.keyword.dto.response.QKeywordCustomAgeResponseDto;
-import com.querydsl.core.types.ExpressionUtils;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
@@ -34,27 +33,18 @@ public class KeywordRepositoryQueryImpl implements KeywordRepositoryQuery {
      */
     @Override
     public List<KeywordCustomAgeResponseDto> readTop10KeywordsByAgeGroup(int minAge, int maxAge) {
-
-       return jpaQueryFactory.select(
-                new QKeywordCustomAgeResponseDto(
-                    keyword.id,
-                    keyword.name,
-                    ExpressionUtils.as(
-                        select(userKeyword.id.count())
-                            .from(userKeyword)
-                            .join(userKeyword.user, user)
-                            .where(user.age.between(minAge, maxAge)
-                                    .and(userKeyword.keyword.id.eq(keyword.id))),
-                        "keywordCount")
-                ))
+        return jpaQueryFactory.select(
+            new QKeywordCustomAgeResponseDto(
+                keyword.id,
+                keyword.name,
+                Expressions.numberTemplate(Long.class, "COUNT({0})", userKeyword.id).as("keywordCount")
+            ))
             .from(userKeyword)
             .join(userKeyword.keyword, keyword)
             .join(userKeyword.user, user)
             .where(user.age.between(minAge, maxAge))
             .groupBy(keyword.id, keyword.name)
-            .orderBy(
-                Expressions.numberPath(Long.class, "keywordCount").desc()
-            )
+            .orderBy(Expressions.numberPath(Long.class, "keywordCount").desc())
             .limit(10)
             .fetch();
     }
