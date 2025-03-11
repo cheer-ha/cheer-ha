6af75b02bb1f8c2097acc8554b5d6ca4e9979caf
lcy <plmokn48@daum.net>
2025-03-12 01:08:18 +0900

    🐛 fix: refreshToken으로 accessToken 재생성 시 쿠키는 변화없던 오류 수정

--- src/main/java/com/project/cheerha/domain/auth/dto/response/RefreshAccessTokenResponseDto.java
+++ src/main/java/com/project/cheerha/domain/auth/dto/response/RefreshAccessTokenResponseDto.java
@@ -1,9 +1,12 @@
 package com.project.cheerha.domain.auth.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record RefreshAccessTokenResponseDto(
-        String accessToken, String message
+        String accessToken, String message, String refreshToken
 ) {
-    public static RefreshAccessTokenResponseDto toDto(String accessToken) {
-        return new RefreshAccessTokenResponseDto(accessToken, "AccessToken 재발급 성공");
+    public static RefreshAccessTokenResponseDto toDto(String accessToken, String refreshToken) {
+        return new RefreshAccessTokenResponseDto(accessToken, "AccessToken 재발급 성공", refreshToken);
     }
 }
