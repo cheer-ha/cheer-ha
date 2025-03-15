a3071768b22629d21cb35f6f730d99eabd64940e
backswan0 <rnrwprh3434@gmail.com>
2025-03-16 02:33:21 +0900

    :white_check_mark: test: 알림 관련 테스트 코드 수정
    - 로직이 변경됨에 따라 Notification 관련 테스트 3가지 또한 모두 수정
    
    (1) NotificationDataFetchingTaskHandlerTest
    (2) NotificationSenderTaskHandlerTest
    (3) NotificationServiceTest

--- src/test/java/com/project/cheerha/domain/notification/scheduler/NotificationSenderTaskHandlerTest.java
+++ src/test/java/com/project/cheerha/domain/notification/scheduler/NotificationSenderTaskHandlerTest.java
@@ -1,52 +1,57 @@
-//package com.project.cheerha.domain.notification.scheduler;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.Mockito.times;
-//import static org.mockito.Mockito.verify;
-//
-//import com.project.cheerha.common.email.sender.NotificationEmailSender;
-//import com.project.cheerha.common.scheduler.strategy.ScheduleStrategy;
-//import com.project.cheerha.common.scheduler.strategy.SpecificTimeStrategy;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//
-//@ExtendWith(MockitoExtension.class)
-//class NotificationSenderTaskHandlerTest {
-//
-//    @Mock
-//    private NotificationEmailSender notificationEmailSender;
-//
-//    @Test
-//    @DisplayName("성공 - getTaskType 메서드가 'sendNotificationEmail' 반환")
-//    void succeedsToGetTaskType() {
-//
-//        // then
-//        String expectedTaskType = "sendNotificationEmail";
-//
-//        assertThat(actualTaskType).isEqualTo(expectedTaskType);
-//    }
-//
-//    @Test
-//    @DisplayName("성공 - handle 메서드가 sendNotificationEmails 호출")
-//    void succeedsToInvokeSendNotificationEmails() {
-//
-//        // then
-//        // sendNotificationEmails() 메서드가 1번 호출되었는지 확인
-//        verify(notificationEmailSender, times(1)).sendNotificationEmails();
-//    }
-//
-//    @Test
-//    @DisplayName("성공 - getScheduleStrategy 메서드가 SpecificTimeStrategy 반환")
-//    void succeedsToGetScheduleStrategy() {
-//        // when
-//        ScheduleStrategy strategy = notificationSenderTaskHandler.getScheduleStrategy();
-//
-//        // then
-//        // 반환된 ScheduleStrategy가 SpecificTimeStrategy 클래스의 인스턴스인지 검증
-//        assertThat(strategy.getClass()).isEqualTo(SpecificTimeStrategy.class);
-//    }
-//}
+package com.project.cheerha.domain.notification.scheduler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.project.cheerha.common.email.sender.NotificationEmailSender;
+import com.project.cheerha.common.scheduler.strategy.ScheduleStrategy;
+import com.project.cheerha.common.scheduler.strategy.SpecificTimeStrategy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationSenderTaskHandlerTest {
+
+    @Mock
+    private NotificationEmailSender notificationEmailSender;
+
+    @InjectMocks
+    private NotificationSenderTaskHandler notificationSenderTaskHandler;
+
+    @Test
+    @DisplayName("성공 - getTaskType 메서드가 'sendNotificationEmail' 반환")
+    void succeedsToGetTaskType() {
+        // when
+        String actualTaskType = notificationSenderTaskHandler.getTaskType();
+
+        // then
+        String expectedTaskType = "sendNotificationEmail";
+        assertThat(actualTaskType).isEqualTo(expectedTaskType);
+    }
+
+    @Test
+    @DisplayName("성공 - handle 메서드가 sendNotificationEmails 호출")
+    void succeedsToInvokeSendNotificationEmails() {
+        // when
+        notificationSenderTaskHandler.handle(null);  // handle method 실행
+
+        // then
+        // sendNotificationEmails() 메서드가 1번 호출되었는지 확인
+        verify(notificationEmailSender, times(1)).sendNotificationEmails();
+    }
+
+    @Test
+    @DisplayName("성공 - getScheduleStrategy 메서드가 SpecificTimeStrategy 반환")
+    void succeedsToGetScheduleStrategy() {
+        // when
+        ScheduleStrategy strategy = notificationSenderTaskHandler.getScheduleStrategy();
+
+        // then
+        assertThat(strategy).isInstanceOf(SpecificTimeStrategy.class);
+    }
+}
